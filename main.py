144e269ae071348c223502754c5b30373942ff7c
Michael Florya <mih2303@mail.ru>
2024-11-15 11:49:52 +0300

    create first window

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+from kivy.app import App
+from kivy.config import Config
 
+
+Config.set("graphics", "width", "400")
+Config.set("graphics", "height", "700")
+Config.set("graphics", "resizable", "0")
+
+
+class FirstApp(App):
+    pass
+
+
+if __name__ == "__main__":
+    FirstApp().run()
